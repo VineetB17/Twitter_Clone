2711e58956388fb63389ee043b29d1b199e9f64e
Vineet Banashankari <125369374+VineetB17@users.noreply.github.com>
2025-02-13 21:15:56 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      width: {
+        '8/10': '80%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
